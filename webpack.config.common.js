29cf81f74c79e33ea23670143f92ef27b5876e64
Sen Lu <swei.lu1018@gmail.com>
2020-12-29 21:38:57 -0800

    inital setup and git push

---
+++ webpack.config.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+   entry: './src/index.jsx',
+	output: {
+		filename: 'bundle.[hash].js',
+		path: path.resolve(__dirname, 'dist')
+
+   },
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+   },
+   plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src', './index.html'),
+			filename: 'index.html'
+      }),
+      new CleanWebpackPlugin()
+	]
+}
